8e91fb36d58050dd01e3457294760a9e7fcb55ab
Cesar <cesiha13@gmail.com>
2025-08-21 12:29:37 -0300

    Implementação  do CORS para corrigir erro no frontend

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const postRoutes = require('./routes/postRoutes');
-
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 app.use('/api', postRoutes); 
 
 app.get('/', (req, res) => {
