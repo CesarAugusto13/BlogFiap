3911ba1b99453145f77160db90a103048912a5ea
Cesar <cesiha13@gmail.com>
2025-12-02 18:10:59 -0300

    Correção de rotas

--- src/routes/professorRoutes.js
+++ src/routes/professorRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
+
 const professorController = require('../controllers/ProfessorController');
-const authMiddleware = require('../middlewares/authMiddleware'); // opcional, proteja rotas
+const auth = require('../middlewares/auth'); // arquivo auth.js
 
 router.post('/register', professorController.register);
 router.post('/login', professorController.login);
 
-// rotas administrativas (protegidas por token)
-router.get('/', /*authMiddleware,*/ professorController.list);
-router.get('/:id', /*authMiddleware,*/ professorController.getById);
-router.patch('/:id', /*authMiddleware,*/ professorController.update);
-router.delete('/:id', /*authMiddleware,*/ professorController.remove);
+router.get('/', auth, professorController.list);
+router.get('/:id', auth, professorController.getById);
+router.patch('/:id', auth, professorController.update);
+router.delete('/:id', auth, professorController.remove);
 
 module.exports = router;
